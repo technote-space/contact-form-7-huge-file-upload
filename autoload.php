0aef9ba6061e9076cc9cc3f6cd9701b6e39d82ee
Technote <technote.space@gmail.com>
2019-07-23 13:15:08 +0900

    feat: rename plugin file

---
+++ autoload.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Contact Form 7 huge file upload
+Plugin URI: https://github.com/technote-space/contact-form-7-huge-file-upload
+Description: Add function to upload huge file
+Author: Technote
+Version: 1.3.8
+Author URI: https://technote.space
+Text Domain: cf7-hfu
+Domain Path: /languages/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( defined( 'CF7_HFU' ) ) {
+	return;
+}
+
+define( 'CF7_HFU', 'CF7_HFU' );
+
+@require_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+WP_Framework::get_instance( CF7_HFU, __FILE__ );
